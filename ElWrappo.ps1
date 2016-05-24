25ca9efb07a31950d7682402fa30418f31b1b2e4
Dalle85 <victor.dahlberg@officeitpartner.se>
2016-05-25 00:30:16 +0200

    Update ElWrappo.ps1

--- ElWrappo.ps1
+++ ElWrappo.ps1
@@ -1,48 +1,36 @@
 <#
-
 .DESCRIPTION
 This is a PowerShell-wrapper for MDT that will install any *.exe or *.msi file located within the source-folder,
-so just throw one, or multiple, executables inside this folder and the script will try to install them.
+so just throw any (or multiple) executable inside the source-folder and the script will try to install it.
 
-However, even if the script will run through any executables inside the source-folder, we will only be
-passing the one argument, so for that reason there should only be one application inside the source-folder,
-unless all files share the same switches for a silent installation.
+However, even though the script potentially could run through multiple executables stored within the source-folder,
+the reason we use this script is so that we don't have to worry about changing the name scripts/commandlines every-
+time we donload a new version of an application. Unless all files share the same switches for a silent installation.
 
 .USAGE
-Make sure that the folder structure looks something like this, then import the application and give it a good name.
+Make sure that the folder structure looks something like this, give the Application a good name and then import
+the folder in your MDT console.
 
-C:\
-├─── Source
-│    └─── MyExecutable.msi
-└─── Install_Wrapper.ps1
+E:.
+└───Install - Application - x64
+    ├───Source
+    │   └───MyApplication.exe
+    └───ElWrappo.ps1
 
-Set the install command to:
-PowerShell.exe –ExecutionPolicy ByPass -WindowStyle Hidden –File ElWrappo.ps1
-
-Logfile will always be named after the folder where the script is located, let's say it would be stored in:
-"\\SERVER\MDTProduction$\Applications\Install - 7-Zip (Latest) - x64"
-In that case the filename will be "Install - 7-Zip (Latest) - x64.txt"
+Set the install command to: PowerShell.exe –ExecutionPolicy ByPass -WindowStyle Hidden –File ElWrappo.ps1
+The logfile will be named after what you named your application folder.
 
 .NOTES
 Don't ask for help, I'll ask you!
 
 .AUTHOR
 Dalle, 2016-05-24, inspired by other Swedish geeks
-
 #>
 
-
-# ---------------------------------------------------------------------------------------------------------------------------
 # Set switches to perform a silent installation
-# ---------------------------------------------------------------------------------------------------------------------------
-
 $Switches = "/S"
 
-
-# ---------------------------------------------------------------------------------------------------------------------------
 # Set some variables and then try to load up the TaskSequence Environment and then start logging
-# ---------------------------------------------------------------------------------------------------------------------------
-
 $SourcePath = "$PSScriptRoot\Source"
 $ScriptName = Split-Path $PSScriptRoot -Leaf
 
@@ -61,11 +49,7 @@ catch {
 
 Start-Transcript -Path $LogFile -Force
 
-
-# ---------------------------------------------------------------------------------------------------------------------------
-# Output some information to the log-file
-# ---------------------------------------------------------------------------------------------------------------------------
-
+#Output some shit to the log
 Write-Output ""
 Write-Output "$ScriptName - ScriptDir: $PSScriptRoot"
 Write-Output "$ScriptName - SourcePath: $SourcePath"
@@ -74,11 +58,7 @@ Write-Output "$ScriptName - Integration with MDT(LTI/ZTI): $MDTIntegration"
 Write-Output "$ScriptName - Log: $LogFile"
 Write-Output ""
 
-
-# ---------------------------------------------------------------------------------------------------------------------------
-# Search source-path for files ending with .msi or .exe, then try to install them one at a time and finally stop logging
-# ---------------------------------------------------------------------------------------------------------------------------
-
+#Start Wrapping
 $Installers = Get-ChildItem "-Path $SourcePath -Recurse –Include *.msi, *.exe"
 $Installers | % {
 
